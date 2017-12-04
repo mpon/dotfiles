d01469ec839562ea58e336654596fe7f207a69eb
Masato Ohshima <m_ohshima@r.recruit.co.jp>
2017-12-05 08:24:21 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+DIR=$(cd $(dirname $0); pwd)
+
+for f in .??*
+do
+  [[ "$f" == ".git" ]] && continue
+  [[ "$f" == ".gitignore" ]] && continue
+  [[ "$f" == ".DS_Store" ]] && continue
+
+  echo "$f"
+  rm ~/$f
+  ln -sf $DIR/$f ~/$f
+done
+
+chsh -s "$(which zsh)"
